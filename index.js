4dcec1419885660be73e0830e5d15e24aecc5a16
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-25 15:27:53 -0400

    Imports and uses CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+
 
 const routeTasks = require('./src/routes/tasks');
 
