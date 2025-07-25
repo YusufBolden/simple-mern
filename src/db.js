81b330b8a039c128eb428be875827a8d4b08ed7f
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-25 14:53:50 -0400

    Adds mongo uri to db

--- src/db.js
+++ src/db.js
@@ -1,5 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect('mongodb://localhost/simple-mern');
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 module.exports = mongoose;
